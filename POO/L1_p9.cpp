7a65b7ef5f5ad09311962c6ed68db85d06dc7fb8
Andra Pescaru <87182577+AndraPescaru@users.noreply.github.com>
2021-11-03 21:23:56 +0200

    Update and rename L1_p8.cpp to L1_p9.cpp

--- POO/L1_p9.cpp
+++ POO/L1_p9.cpp
@@ -1,6 +1,6 @@
 /*
  Programmer : Anamaria Andra Pescaru 
- Cerinta : Să se scrie o clasă Carte cu următoarele date membre: titlu, nume_autor, an_lansare, nr_pagini, pret. Se vor instanția maxim 5 obiecte din această clasă folosind un vector cu alocare statică de memorie. Se citește de la tastatură numărul de cărți pe care dorim să îl folosim. Ulterior, pentru fiecare carte se vor citi, de pe rânduri diferite datele membre în ordinea enunțată mai sus.
+ Cerinta : Să se scrie o clasă Carte cu următoarele date membre: titlu, nume_autor, an_lansare, nr_pagini, pret. Se vor instanția maxim 5 obiecte din această clasă folosind un vector cu alocare dinamica de memorie. Se citește de la tastatură numărul de cărți pe care dorim să îl folosim. Ulterior, pentru fiecare carte se vor citi, de pe rânduri diferite datele membre în ordinea enunțată mai sus.
 
 Se va citi o comandă de la tastatură. În funcție de valoarea acesteia se vor executa diferite acțiuni:
 Se citește de la tastatură un nume de autor. Se afișează titlurile tuturor cărților scrise de acest autor, separate de virgulă și spațiu. Pentru simplitate, ultimul titlu va fi, de asemenea, urmat de virgulă și spațiu.
